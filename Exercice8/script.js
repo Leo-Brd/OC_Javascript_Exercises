1560574164a22b1ba27133a0146941e751bbc512
Leo-Brd <leobordet.pro@gmail.com>
2025-10-17 23:26:22 +0200

    feat: add exercise 8

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,11 @@
-//Votre code ici
+function add(a, b) {
+    if (typeof a == 'string') {
+        a = parseFloat(a);
+    }
+    if (typeof b == 'string') {
+        b = parseFloat(b);
+    }
+    return a + b;
+}
 
  export default add;
