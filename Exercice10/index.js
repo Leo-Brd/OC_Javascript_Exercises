f99ebea7f1a38f09eaddf946f0bf92d6bb3b9a0d
Leo-Brd <leobordet.pro@gmail.com>
2025-10-17 23:33:13 +0200

    feat: add exercise 10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,14 @@
-//votre code ici
 
-export default printNumbers
+function printNumbers(n) {
+    let string = '';
+    for (let i = 1; i <= n; i++) {
+        if (i == n) {
+            string += i;
+            break;
+        }
+        string += i + ' ';
+    }
+    return string;
+}
+
+export default printNumbers;
